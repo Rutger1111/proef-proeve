d023f4f35c4c9b69045e47744de9f9503ecc1fae
BlubBlub <32533@ma-web.nl>
2025-02-24 09:32:48 +0100

    added buttons for button instatiation
    when buttons so you can select differents closeds

--- DressUp/Assets/Scripts/WardrobeScripts/WardrobeInstantiator.cs
+++ DressUp/Assets/Scripts/WardrobeScripts/WardrobeInstantiator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
@@ -9,16 +10,49 @@ public class WardrobeInstantiator : MonoBehaviour
     public Transform canvasTransform;
     public List<Wardrobe> wardrobes;
     public SpriteHandler spriteHandler;
+
+    private List<GameObject> buttonsList = new List<GameObject>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (int index = 0; index <= wardrobes.Count - 1; index ++){
+        /*for (int index = 0; index <= wardrobes.Count - 1; index ++){
             GameObject gameObject = Instantiate(gridPrefab,canvasTransform);
             gameObject.transform.parent = canvasTransform.GetChild(1);
             for (int I = 0; I < wardrobes[index].clothesTextures.Count; I ++){
                 GameObject gameObject1 = Instantiate(boxiePrefab, gameObject.transform);
                 gameObject1.GetComponent<ClotheSettings>().cloth = wardrobes[index].clothesTextures[I];
             }
+        }*/
+    }
+    private void Update()
+    {
+        foreach (var button in buttonsList.ToList())
+        {
+            if (button == null)
+            {
+                buttonsList.Remove(button);
+            }
         }
+       
+    }
+    public void buttonInstatiate(int buttonIndex)
+    {
+        foreach (var button in buttonsList) 
+        { 
+            Destroy(button);
+        }
+        
+        GameObject gameObject = Instantiate(gridPrefab, canvasTransform);
+
+        buttonsList.Add(gameObject);
+
+            gameObject.transform.parent = canvasTransform.GetChild(1);
+            for (int I = 0; I < wardrobes[buttonIndex].clothesTextures.Count; I++)
+            {
+                GameObject gameObject1 = Instantiate(boxiePrefab, gameObject.transform);
+                gameObject1.GetComponent<ClotheSettings>().cloth = wardrobes[buttonIndex].clothesTextures[I];
+            }
+        
+
     }
 }
