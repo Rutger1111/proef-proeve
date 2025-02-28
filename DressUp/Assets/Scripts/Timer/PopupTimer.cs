f5eb394b322a7079109d7dbee85fa6661bba75c4
Rutger1111 <djrutgervandelaar@gmail.com>
2025-02-28 01:08:33 +0100

    fixes and popuptimer
    heb timer voor popup gemaakt visueel
    en heb extra haar aan wardrobe gevoegd :)

--- DressUp/Assets/Scripts/Timer/PopupTimer.cs
+++ DressUp/Assets/Scripts/Timer/PopupTimer.cs
@@ -4,14 +4,23 @@ public class PopupTimer : MonoBehaviour
 {
     public float timer = 3;
     public float resetTimer = 3;
-
+    [SerializeField] private TMPro.TMP_Text popupTimerText;
     public GameObject displayModel;
     
     void Update()
     {
         timer -= Time.deltaTime;
+
         if (timer <= 0){
             displayModel.SetActive(false);
+        }
+        else{
+            if (timer >= 1){
+                popupTimerText.text = timer.ToString("F0");
+            }
+            else{
+                popupTimerText.text = timer.ToString("F1");
+            }
         }
     }
 
